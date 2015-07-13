f057f7d9a813fa13e041115899af2e1e3cb3692d
student <maciejlozowski@tieto.com>
2015-07-13 16:00:05 +0200

    thread1

--- _threads_1/src/main.c
+++ _threads_1/src/main.c
@@ -5,7 +5,7 @@
 
 void *myThread(void *arg){
 	int i;
-	int idPotomnego = fork();
+	int idPotomnego = fork(); //dokladna kopia procesu macierzystego(tworzy proces potomny z danego procesu)ale otrzymuje nowe PID i nowe zasoby. Dodoatkowo proces macierzysty otrzymuje kod powrotou funkcji fork() równy PID-owi procesu potomnego, a proces potomny otrzymuje kod powrotu funkcji fork() równy 0.
 	if (idPotomnego == 0) {
 	    printf("Jesteś w procesie potomnym, jego ID = %d\n", getpid());
 	} else {
@@ -22,6 +22,9 @@ int main(int argc, const char *argv[])
 {
 	pthread_t firstThread;
 	pthread_create ( &firstThread, NULL, myThread, NULL);
+	while(1){
+		sleep(10); // w shellu ps aux |grep  pthread_cos-nazwa  .... gdb -p (pid procesu)(sledzimy procesy) (gdb) t a a bt (sprawdza ilosc watkow)
+	} //fork() kopiuje tylko dany watek, ktory go wywolal i tylko go bedzie sledzil...
 	pthread_join(firstThread, NULL);
 	exit(0);
 	return 0;
